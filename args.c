db3a237c6b266b4a591c1de42f5e528d63411874
Nehuen Laje <nlaje@yahoo.com.ar>
2021-07-01 11:46:25 -0300

    tercer commit

--- args.c
+++ args.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hola, %s\n", argv[1]);
+  printf("Hola, %s\n", argv[3]);
+  printf(", %s\n", argv[2]);
+  printf("y %s\n", argv[1]);
   return 0;
 }
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+  int n = 255;
+  printf("%d\t%x\n", n, n);
+  return 0;
+}
