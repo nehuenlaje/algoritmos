81539c1d31419a54bb3b1a335bf2b65bb2089e39
Nehuen Laje <nlaje@yahoo.com.ar>
2021-07-01 09:45:57 -0300

    primer commit

---
+++ hola_mundo.c
@@ -0,0 +1,13 @@
+// hello.c
+#include <stdio.h>
+
+
+
+int main(void) {
+    int i;
+    int count = 10; // number of times to repeat
+
+    for (i = 0; i<count;i++){
+        printf("%s\n","hello world" );  
+
+}
