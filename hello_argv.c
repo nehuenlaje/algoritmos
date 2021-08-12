1f1f27b167df5443cc980e1b4d98b547fb706adf
Nehuen <mamdwkdkmwdmkam@lalala.com>
2021-08-12 12:09:55 -0300

    asd

---
+++ hello_argv.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+    int count = atoi(argv[1]);
+    // int count = i;
+
+    for (int i = 0;i<count;i++)
+        printf("%s\n","hello world" );
+
+}
